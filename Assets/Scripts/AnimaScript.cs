bf8751c0e76851113b4b7610a269a254fde0e415
logonrm <logonrm@fiap.com.br>
2018-02-05 22:58:16 -0200

    first commit

---
+++ Assets/Scripts/AnimaScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimaScript : MonoBehaviour {
+	public float velocidade;
+	SpriteRenderer spriteRender;
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		spriteRender = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mover_x = Input.GetAxisRaw ("Horizontal") * velocidade * Time.deltaTime;
+		transform.Translate (mover_x, 0.0f, 0.0f);
+		//orientacao sprite
+		if (mover_x > 0.0f) {
+			spriteRender.flipX = false;
+		}
+		else if(mover_x < 0.0f) {
+			spriteRender.flipX= true;
+				
+			
+		}
+		animator.SetFloat ("pMover", Mathf.Abs (Input.GetAxisRaw ("Horizontal")));
+	}
+}
